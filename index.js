4b378814949c3b33bd7ef2cfd3be26634e0e60b9
Jacky95000 <jacky.bonnot@lapiscine.pro>
2025-03-18 14:32:51 +0100

    header

---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+    function toggleMenu() {
+        const navMenu = document.querySelector('.navMenu');
+        navMenu.style.display = (navMenu.style.display === 'block' ? 'none' : 'block');
+    }
+</script>
